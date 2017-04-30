28a7ce6f502fff19a5f3dc9a27b672c2615f655a
richard-dev <richardl890@gmail.com>
2017-04-30 00:11:11 -0400

    Final commit.

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -1,14 +1,6 @@
 function loadData() {
-
     var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
 
     // get street view pic
     var streetStr = $('#street').val();
@@ -21,14 +13,67 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + streetViewUrl + '">');
 
     // get nyt article headlines
-    $.getJSON( "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + address, function ( data ) {
-        var items = []
-        $.each( data, function( key, val ) {
-            
-        });
-    });
+    getNYT(cityStr, address);
+
+    // get wiki
+    getWiki(cityStr);
 
     return false;
 };
+
+function getNYT(cityStr, address) {
+    // getJSON example.
+    // API key: 8287e25b092b427191a1e3ebfcccffbc
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var nytimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=8287e25b092b427191a1e3ebfcccffbc&sort=newest&q=" + address
+    
+    // clear out old data before new request
+    $nytElem.text("");
+
+    $.getJSON(nytimesUrl, function (data) {
+        $nytHeaderElem.text('New York Shitty Times Articles About ' + cityStr);
+
+        var articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        };   
+    }).error(function(){
+        $nytHeaderElem.text('New York Shitty Times Took a Shit.');
+    });
+}
+
+function getWiki(cityStr) {
+    // .Ajax example.
+    var $wikiElem = $('#wikipedia-links');
+    var $wikiHeader = $('#wikipedia-header');
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + cityStr + '&callback=wikiCallback';
+
+    // clear out old data before new request
+    $wikiElem.text("");
+
+    // error handling for jsonp since error doesn't work
+    var wikiRequestTimeout = setTimeout(function() {
+        $wikiElem.text('Failed to get wiki resources.');
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        success: function (response) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
+}
 
 $('#form-container').submit(loadData);
